0609ae2e05b806783a80286ea81f739c1e28a565
Alexander Garth Odell <aode@dl-iad.office.gameduell.de>
2015-07-16 14:24:12 +0200

    added full skeleton

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,33 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    jade = require('gulp-jade'),
+    livereload = require('gulp-livereload'),
+    express = require('express');
 
-var paths = {
-    jade: ['app/templates/**/*.jade', '!app/templates/partials/**/*.jade'],
-    images: 'client/img/**/*'
-},
-    port = 3000;
+var port = 3000,
+    paths = {
+        jade: ['src/templates/**/*.jade', '!src/templates/partials/**/*.jade'],
+        dest: 'build/'
+    };
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
+    livereload.listen();
     gulp.watch(paths.jade, ['jadeTask']);
 });
 
+gulp.task('jadeTask', function(){
+    gulp.src(paths.jade)
+        .pipe(jade({pretty:'    '}))
+        .pipe(gulp.dest(paths.dest))
+        .pipe(livereload());
+});
+
 gulp.task('server', function() {
-    var app = connect();
+    var app = express();
+    app.use(express.static(__dirname + '/build'));
+    app.use('/bower_components',  express.static(__dirname + '/bower_components'));
     app.listen(port);
 });
 
-// gulp.task('default', ['watch', 'server']);
-gulp.task('default', function(){
-    gutil.log('Sup');
-});
+gulp.task('default', ['jadeTask', 'watch', 'server']);
